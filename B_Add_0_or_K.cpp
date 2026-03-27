abe9b9e43b0523a00454268949428d323f60d49c
Akhilesh kumar yadav <akhileshyadav88470@gmail.com>
2026-03-27 18:29:27 +0530

    codeforces-B_Add_0_or_K

--- B_Add_0_or_K.cpp
+++ B_Add_0_or_K.cpp
@@ -1,11 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-    int n,k;
+    long long n,k;
     cin>>n>>k;
-    vector<int>a(n);
-    for(int i=0;i<n;i++)cin>>a[i];
-    
+    vector<long long>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        long long x = a[i] % (k+1);
+        a[i] += x*k;
+    }
+    for(auto it:a){
+        cout<<it<<" ";
+
+    }
+    cout<<"\n";
 
 }
 int main(){
