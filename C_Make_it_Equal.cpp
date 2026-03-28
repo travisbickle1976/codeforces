13f70450adf5da6c88c3cde780098defbe86bacd
Akhilesh kumar yadav <akhileshyadav88470@gmail.com>
2026-03-28 09:53:48 +0530

    codeforces-C_Make_it_Equal

--- C_Make_it_Equal.cpp
+++ C_Make_it_Equal.cpp
@@ -1,38 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    
+    map<int ,int>hash;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        hash[x%k]++,hash[k-x%k]++;
+      
+    }
+    
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        hash[x%k]--,hash[k-x%k]--;
+        
+    }
+    for(auto it:hash){
+        if(it.second){
+        cout<<"NO"<<endl;
+        return;
+        }
+    }
+    cout<<"YES\n";
+
+}
+
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int n,m;
-        cin>>n>>m;
-        vector<int>jake,black;
-        for(int i=0;i<n;i++){
-            int x;
-            cin>>x;
-            jake.push_back(x);
-        }
-         for(int i=0;i<n;i++){
-           int y;
-            cin>>y;
-            black.push_back(y);
-        }
-        bool tom = false;
-        sort(jake.begin(),jake.end());
-        sort(black.begin(),black.end());
-         for(int i=0;i<n;i++){
-           int diff = abs((black[i]-jake[i]));
-           if(diff > m){
-            cout<<"NO"<<endl;
-            tom = true;
-           }
-         }
-         if(tom==false){
-            cout<<"YES"<<endl;
-         }
-        
-
-
+     solve();
     }
     return 0;
 }
