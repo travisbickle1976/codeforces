66ab9170ed1765a51f8236a671007bed8e09ad00
Akhilesh kumar yadav <akhileshyadav88470@gmail.com>
2026-04-01 13:34:17 +0530

    codeforces-solution

---
+++ E_MEX_Count.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    vector<int>cnt[n];
+    for(int i=0;i<n;i++){
+        cnt[a[i]]++;
+    }
+    int count = 0;
+   
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+}
