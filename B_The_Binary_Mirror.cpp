44e0b31eabb6c6c18f18a1fe6d48fab350a7c232
Akhilesh kumar yadav <akhileshyadav88470@gmail.com>
2026-04-17 03:20:55 +0530

    codeforces-B_The_Binary_Mirror.cpp

---
+++ B_The_Binary_Mirror.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    if(n%4==1 || n%4==2) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
