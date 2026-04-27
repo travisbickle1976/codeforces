eebb1dad4b83613477ddd5846fd03778b9093a23
Akhilesh kumar yadav <akhileshyadav88470@gmail.com>
2026-04-27 14:11:17 +0530

    codeforces--A_String.cpp

---
+++ A_String.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int t;
+  cin >> t;
+ 
+  while (t--) {
+    string s;
+    cin>>s;
+    int cnt = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if(s[i]=='1'){
+            cnt++;
+        }
+    }
+ 
+    cout<<cnt<< endl;
+  }
+  
+  return 0;
+}
